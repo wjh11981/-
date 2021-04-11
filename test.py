5b8d5f1e7e67a152ac57a5aec96b7505334d725a
wjh11981 <1473135668@qq.com>
2021-04-11 17:48:50 +0800

    second commit

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser =webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+   #测试前运行的
+    def setUp(self):
+        self.broswer=webdriver.Firefox()
+    #测试后运行的
+    def tearDown(self):
+        self.broswer.quit()
+    #前缀为test的都为测试代码
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.broswer.get('http://localhost:8000')
+        self.assertIn('To-Do',self.broswer.title)
+        self.fail('Finish the test!')
 
-assert 'Django' in browser.title
-
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
