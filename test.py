d0dd1915e04b28aed3b3b3d59d8f94bdf6b91f42
wjh11981 <1473135668@qq.com>
2021-04-16 14:31:58 +0800

    Functional test now checks we can input a to-do

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser=webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
+
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
