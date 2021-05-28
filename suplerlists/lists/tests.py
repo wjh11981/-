b8a7f1dfc0e4752ef64f48a8230b4330c8f17a0d
wjh11981 <1473135668@qq.com>
2021-05-28 13:55:54 +0800

    new commit

--- suplerlists/lists/tests.py
+++ suplerlists/lists/tests.py
@@ -3,7 +3,7 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item,List
 # Create your tests here.
 
 
@@ -18,24 +18,23 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(),0)
 
-    # def test_displays_all_list_items(self):
-    #     Item.objects.create(text='itemey 1')
-    #     Item.objects.create(text='itemey 2')
-    #
-    #     response=self.client.get('/')
-    #
-    #     self.assertIn('itemey 1',response.content.decode())
-    #     self.assertIn('itemey 2',response.content.decode())
-
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_=List()
+        list_.save()
+
         first_item =Item()
         first_item.text='The first (ever) list item'
+        first_item.list=list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list=list_
         second_item.save()
+
+        saved_list=List.objects.first()
+        self.assertEqual(saved_list,list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(),2)
@@ -43,7 +42,9 @@ class ItemModelTest(TestCase):
         first_saved_item=saved_items[0]
         second_saved_item=saved_items[1]
         self.assertEqual(first_saved_item.text,'The first (ever) list item')
+        self.assertEqual(first_saved_item.list,list_)
         self.assertEqual(second_saved_item.text,'Item the second')
+        self.assertEqual(second_saved_item.list,list_)
 
 
 class ListViewTest(TestCase):
@@ -71,3 +72,9 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response=self.client.post('/lists/new',data={'item_text':'A new list item'})
         self.assertRedirects(response,'/lists/the-only-list-in-the-world/')
+
+class ListViewTest(TestCase):
+    def test_displays_all_items(self):
+        list_=List.objects.create()
+        Item.objects.create(text='itemey 1',list=list_)
+        Item.objects.create(text='itemey 2',list=list_)
