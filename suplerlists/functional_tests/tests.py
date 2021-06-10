b35a0023b3c5472889da2af38abde42a6b78d090
wjh11981 <1473135668@qq.com>
2021-06-10 20:47:36 +0800

    first steps of FT for layout +styling

--- suplerlists/functional_tests/tests.py
+++ suplerlists/functional_tests/tests.py
@@ -76,3 +76,13 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('Buy milk', page_text)
 
+    def test_layout_and_styling(self):
+        inputbox.send_keys('testing')
+        inputbox.send_keys(keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
